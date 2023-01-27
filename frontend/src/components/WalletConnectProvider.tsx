6932f6defb6151272e2b2a31d89e9e19f649ebf8
Linkie Link <linkielink.dev@gmail.com>
2023-01-27 15:19:31 +0100

    fix: fix auto reconnect

--- frontend/src/components/WalletConnectProvider.tsx
+++ frontend/src/components/WalletConnectProvider.tsx
@@ -1,39 +1,28 @@
 import CircularProgress from './CircularProgress'
 import styles from './WalletConnectProvider.module.scss'
 import { ChainInfoID, WalletID, WalletManagerProvider } from '@marsprotocol/wallet-connector'
-import i18next from 'i18next'
-import { FC, useState } from 'react'
+import { FC } from 'react'
 
 const enabledWallets = [WalletID.Keplr, WalletID.Cosmostation]
 
 const defaultChainId = ChainInfoID.MarsAres1
 
 const CosmosWalletConnectProvider: FC<WrapperComponent> = ({ children }) => {
-  const [i18nInit, setI18nInit] = useState(false)
-
-  i18next.on('initialized', () => {
-    setI18nInit(true)
-  })
-
-  if (i18nInit) {
-    return (
-      <WalletManagerProvider
-        defaultChainId={defaultChainId}
-        enabledWallets={enabledWallets}
-        persistent
-        renderLoader={() => (
-          <div className={styles.loader}>
-            <CircularProgress size={30} />
-          </div>
-        )}
-        closeIcon={<div />}
-      >
-        {children}
-      </WalletManagerProvider>
-    )
-  } else {
-    return null
-  }
+  return (
+    <WalletManagerProvider
+      defaultChainId={defaultChainId}
+      enabledWallets={enabledWallets}
+      persistent
+      renderLoader={() => (
+        <div className={styles.loader}>
+          <CircularProgress size={30} />
+        </div>
+      )}
+      closeIcon={<div />}
+    >
+      {children}
+    </WalletManagerProvider>
+  )
 }
 
 export default CosmosWalletConnectProvider
